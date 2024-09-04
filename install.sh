616506619f20c6231f485844049c2ae226b95ed8
alerighi <alessandro.righi@alerighi.it>
2024-09-04 22:03:41 +0200

    add nvim configuration

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 set -xe
-mkdir -p "${HOME}/.local/bin" 
+mkdir -p "${HOME}/.local/bin"
 
 for f in $PWD/bin/*; do
 	ln -sf --backup "$f" "${HOME}/.local/bin"
@@ -10,3 +10,5 @@ done
 ln -sf --backup "${PWD}/config/zshrc" "${HOME}/.zshrc"
 ln -sf --backup "${PWD}/config/vimrc" "${HOME}/.vimrc"
 ln -sf --backup "${PWD}/config/bashrc" "${HOME}/.bashrc"
+ln -sf --backup "${PWD}/config/nvim" "${HOME}/.config/nvim"
+
